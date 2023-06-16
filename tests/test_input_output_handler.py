97238b096e593af58c6f493824538f869d373fe2
FabianTsch <tschohl.fabian@gmail.com>
2023-06-16 20:43:18 +0200

    simple reformat test_input_output_handler

--- tests/test_input_output_handler.py
+++ tests/test_input_output_handler.py
@@ -1,15 +1,12 @@
 import wave
 import numpy as np
 import pyaudio
-import pytest
-import os
-import sys
 import input_output_handler as io
 
 
-def test_read_input_type(capfd):
+def test_read_input_type_not_supported(capfd):
     """
-    Test if the function read_input raises an error if the type of the target is not supportedself.
+    Test if the function read_input raises an error if the type of the target is not supported.
     """
     target = 4.5
     data = io.read_input(target, 1024)
@@ -46,11 +43,13 @@ def test_get_targets_type():
     """
     Test if the function get_targets returns the correct types.
     """
+
     input_filename = "./records/record_30.wav"
     output_filename = "test.wav"
     read_target, write_target = io.get_targets(input_filename, output_filename)
     assert type(read_target) == wave.Wave_read
     assert type(write_target) == wave.Wave_write
+
     input_filename = None
     output_filename = None
     read_target, write_target = io.get_targets(input_filename, output_filename)
