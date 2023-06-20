e922288f46b2a429e9e1611443f60d8f8009027a
FabianTsch <tschohl.fabian@gmail.com>
2023-06-20 21:16:40 +0200

    removed main test

--- tests/test_main.py
+++
@@ -1,8 +0,0 @@
-from main import main
-
-
-def test_main():
-    input_filename = "./records/record_30.wav"
-    output_filename = ".test.wav"
-    main(input_filename, output_filename)
-    assert True
